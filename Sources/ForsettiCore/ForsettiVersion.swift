5d760f159782a92e909067d980134c48d3c39a81
Jim Daley <94642455+flynn33@users.noreply.github.com>
2026-03-04 04:38:01 -0600

    Version Control Automation

--- Sources/ForsettiCore/ForsettiVersion.swift
+++ Sources/ForsettiCore/ForsettiVersion.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 public enum ForsettiVersion {
-    public static let current = SemVer(major: 0, minor: 1, patch: 0)
+    public static let current = SemVer(
+        major: 0, // x-release-please-major
+        minor: 1, // x-release-please-minor
+        patch: 0  // x-release-please-patch
+    )
 }
